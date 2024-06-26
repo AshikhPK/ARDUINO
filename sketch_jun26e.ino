1c3f7486ac43fc4cf7e6f180a22fa15a9603150d
AshikhPK <ashikhhari2004@gmail.com>
2024-06-26 13:37:40 +0530

    Add files via upload

---
+++ sketch_jun26e.ino
@@ -0,0 +1,31 @@
+const int tempSensorpin=A0;
+const int buzz = 8;
+const float tempTh=25.0;
+int sensorValue
+float temperature
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buzz,LOW);
+  degitalWrite(buzz,LOW);
+
+}
+
+void loop() {
+  sensorValue=analogRead(tempSensorpin);
+  temperature=sensorValue*(3.0/1023.0)*10;
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+
+if(temperature>tempTh){
+  digitalWrite(buzz,HIGH);
+  
+  }
+else
+{
+  digitalWrite(buzz,LOW);
+  
+  }
+  delay(100);
+}
